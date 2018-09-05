175723585ad9b77d8785bb49fff0d8e39e8b2c42
huangweiqi <askviky2010@gmail.com>
2018-09-05 23:38:03 +0800

    完善项目代码

---
+++ app/src/main/java/com/abt/mqtt/app/MqttApp.java
@@ -0,0 +1,16 @@
+package com.abt.mqtt.app;
+
+import com.abt.basic.BasicApplication;
+
+/**
+ * @描述： @MqttApp
+ * @作者： @黄卫旗
+ * @创建时间： @05/09/2018
+ */
+public class MqttApp extends BasicApplication {
+
+    @Override
+    public void initComplete() {
+
+    }
+}
--- app/src/main/java/com/abt/mqtt/ui/MainActivity.kt
+++ app/src/main/java/com/abt/mqtt/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.abt.mqtt
+package com.abt.mqtt.ui
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.abt.mqtt.R
 
 class MainActivity : AppCompatActivity() {
 
