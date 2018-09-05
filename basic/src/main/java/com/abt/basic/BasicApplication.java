9ae1c09c3f2c2abf4a8ed0b8df9494869aaabf3e
huangweiqi <askviky2010@gmail.com>
2018-09-05 23:19:21 +0800

    init repo

---
+++ basic/src/main/java/com/abt/basic/BasicApplication.java
@@ -0,0 +1,34 @@
+package com.abt.basic;
+
+import android.app.Application;
+import com.abt.basic.BuildConfig;
+
+/**
+ * @描述： @基类application
+ * @作者： @黄卫旗
+ * @创建时间： @05/09/2018
+ */
+public abstract class BasicApplication extends Application {
+
+    private static BasicApplication sContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sContext = this;
+        init();
+        initComplete();
+    }
+
+    public static final BasicApplication getAppContext() {
+        return sContext;
+    }
+
+    private final void init() {
+        if (BuildConfig.DEBUG) {
+            //DebugManage.initialize(this);
+        }
+    }
+
+    public abstract void initComplete();
+}
