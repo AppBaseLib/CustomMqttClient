f2165adbb92a9336c790baeb9b26ef6d926fbff4
WeiQi <askviky2010@gmail.com>
2018-09-07 11:42:26 +0800

    Update MqttImpl.java

--- mqtt/src/main/java/com/abt/mqtt/impl/MqttImpl.java
+++ mqtt/src/main/java/com/abt/mqtt/impl/MqttImpl.java
@@ -17,6 +17,7 @@ import javax.net.ssl.SSLSocketFactory;
  */
 public class MqttImpl implements IMqtt {
 
+    private static final String TAG = MqttImpl.class.getSimpleName();
     private BaseMqttService mMqttService;
     private MqttConnectionStatus mConnectionStatus = MqttConnectionStatus.NOTCONNECTED_DATADISABLED;
 
@@ -31,14 +32,15 @@ public class MqttImpl implements IMqtt {
     }
 
     public void init(Context context) {
-        buildMqttService(context);
-        connect();
+        LogHelper.d(TAG, "init client");
+        boolean success = buildMqttService(context);
+        if (success) connect();
     }
 
     /**
      * 构建EasyMqttService对象
      */
-    private void buildMqttService(Context context) {
+    private boolean buildMqttService(Context context) {
         mConnectionStatus = MqttConnectionStatus.INITIAL;
         SSLSocketFactory sslSocketFactory = null;
         try {
@@ -46,29 +48,35 @@ public class MqttImpl implements IMqtt {
                     SSLConfig.CRT_PATH, SSLConfig.KEY_PATH, SSLConfig.PASSWORD);
         } catch (Exception e) {
             e.printStackTrace();
+            return false;
         }
         mMqttService = new BaseMqttService.Builder()
                 //设置自动重连
                 .autoReconnect(MqttConfig.AUTO_RECONNECT)
                 //设置不清除回话session 可收到服务器之前发出的推送消息
                 .cleanSession(MqttConfig.CLEAN_SESSION)
+                .retained(MqttConfig.RETAINED)
                 //唯一标示 保证每个设备都唯一就可以 建议 imei
                 .clientId(MqttConfig.CLIENT_ID)
                 //mqtt服务器地址 格式例如：tcp://10.0.261.159:1883
                 .serverUrl(MqttConfig.SERVER_URL)
                 .userName(MqttConfig.USER_NAME)
                 .passWord(MqttConfig.PASSWORD)
+                .timeOut(MqttConfig.TIME_OUT)
                 //心跳包默认的发送间隔
                 .keepAliveInterval(MqttConfig.HEART_BEAT_INTERVAL)
                 //构建出EasyMqttService 建议用application的context
                 .sslSocketFactory(sslSocketFactory)
                 .bulid(context);
+        LogHelper.d(TAG, "SERVER_URL="+MqttConfig.SERVER_URL);
+        return true;
     }
 
     /**
      * 连接Mqtt服务器
      */
     private void connect() {
+        LogHelper.d(TAG, "client inited, start to connect server.");
         mConnectionStatus = MqttConnectionStatus.CONNECTING;
         mMqttService.connect();
     }
@@ -77,6 +85,7 @@ public class MqttImpl implements IMqtt {
      * 订阅主题 这里订阅三个主题分别是"a", "b", "c"
      */
     public void subscribe(String[] topics, int[] qoss) {
+        LogHelper.d(TAG, "subscribe, topics="+topics+", qoss="+qoss);
         // String[] topics = new String[]{"a", "b", "c"};
         // 主题对应的推送策略 分别是0, 1, 2
         // 建议服务端和客户端配置的主题一致
@@ -92,6 +101,7 @@ public class MqttImpl implements IMqtt {
      */
     @Override
     public void close() {
+        LogHelper.d(TAG, "close");
         mMqttService.close();
     }
 
@@ -100,6 +110,7 @@ public class MqttImpl implements IMqtt {
      */
     @Override
     public void disconnect() {
+        LogHelper.d(TAG, "disconnect");
         mConnectionStatus = MqttConnectionStatus.NOTCONNECTED_USERDISCONNECT;
         mMqttService.disconnect();
     }
@@ -120,6 +131,7 @@ public class MqttImpl implements IMqtt {
      */
     @Override
     public void publish(String msg, String topic, int qos, boolean retained) {
+        LogHelper.d(TAG, "publish, msg="+msg+", topic="+topic+", qos="+qos+", retained="+retained);
         mMqttService.publish(msg, topic, qos, retained);
     }
 
